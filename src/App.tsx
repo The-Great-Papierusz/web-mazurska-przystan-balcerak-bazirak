6cd41727de8d9db7c80a5603c5a440ce14b8287b
bartek <bartosz.balcerak@cyfroweszkoly.pl>
2026-03-13 10:11:08 +0100

    dodanie value i name do pol formularza

--- src/App.tsx
+++ src/App.tsx
@@ -10,13 +10,13 @@ function App() {
       <div className="form-container">
 
         <label className="biggerLetters">Imię</label>
-        <input type="text" />
+        <input type="text" name="imie"/>
 
         <label className="biggerLetters">Sprzęt</label>
         <select>
-          <option>Kajak (20zł/h)</option>
-          <option>Rower wodny (35zł/h)</option>
-          <option>Omega (150zł/h)</option>
+          <option value="kajak">Kajak (20zł/h)</option>
+          <option value="rowerWodny">Rower wodny (35zł/h)</option>
+          <option value="omega">Omega (150zł/h)</option>
         </select>
 
 
@@ -25,32 +25,34 @@ function App() {
           type="range"
           min="1"
           max="8"
+          defaultValue="1"
+          name="ileGodzin"
         />
 
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" name="kapok"/>
           Kapok dla dziecka (+5zł)
         </label>
 
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" name="instruktor"/>
           Instruktor (+50zł/h)
         </label>
 
         <p className="biggerLetters">Płatność</p>
 
         <label>
-          <input type="radio" name="payment" />
+          <input type="radio" name="payment" value="karta"/>
           Karta
         </label>
 
         <label>
-          <input type="radio" name="payment" />
+          <input type="radio" name="payment" value="blik"/>
           BLIK
         </label>
 
         <label>
-          <input type="checkbox" />
+          <input type="checkbox" name="regulamin"/>
           Akceptuję regulamin
         </label>
 
